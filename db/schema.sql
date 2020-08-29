638353af8da14e24465c61a991bab3e013abc719
egibbs834 <edwardgibbons34@gmail.com>
2020-08-29 19:54:37 -0400

    app setup; created json, added dependencies

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers;
+id INT auto_increment PRIMARY KEY;
+burger_name VARCHAR (50) NOT NULL;
+devoured BOOLEAN 
